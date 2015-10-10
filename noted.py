31fdcd3ce6cc737b26cfe51c74e64178cb2fc720
bkwatanabe <brett.k.watanabe@gmail.com>
2015-10-09 22:50:37 -0700

    I broke stuff

--- noted.py
+++ noted.py
@@ -1,12 +1,19 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    template = render_template('index.html')
+    if (request.method == 'POST'):
+        newNote = request.form['newNote']
+        return newNote['title']
+    return template
+
+@app.route('/hello')
 def hello_world():
-    return render_template("index.html")
+    return 'Hello World!meowmwow'
 
 
 if __name__ == '__main__':
