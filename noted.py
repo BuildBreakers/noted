ef7f5df3c5cd5dd8463097f17210a1addfe567ff
bkwatanabe <brett.k.watanabe@gmail.com>
2015-12-06 00:00:36 -0800

    Added templates and db plumbing for notes

--- noted.py
+++ noted.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from db_setup import Base, Topic, Note
 
 app = Flask(__name__)
@@ -39,9 +40,42 @@ def deleteTopic(topic_id):
         return redirect(url_for('home'))
     return render_template('delete-topic.html', topic = topic)
 
+
+@app.route('/topic/<int:topic_id>/rename/', methods=['GET', 'POST'])
+def renameTopic(topic_id):
+    topic = session.query(Topic).filter_by(id=topic_id).one()
+    if (request.method == 'POST'):
+        topic.topic = request.form['newTopic']
+        session.commit()
+        return redirect(url_for('home'))
+    return render_template('rename-topic.html', topic=topic)
+
+
+@app.route('/topic/<int:topic_id>/note/<int:note_id>/delete/', methods=['GET', 'POST'])
+def deleteNote(topic_id, note_id):
+    topic = session.query(Topic).filter_by(id=topic_id).one()
+    note = session.query(Note).filter_by(topic_id=topic_id, id=note_id).one()
+    if (request.method == 'POST'):
+        session.delete(note)
+        session.commit()
+        return redirect(url_for('topic', topic_id=topic.id))
+    return render_template('delete-note.html', topic=topic, note=note)
+
+
+@app.route('/topic/<int:topic_id>/note/<int:note_id>/edit/', methods=['GET', 'POST'])
+def editNote(topic_id, note_id):
+    topic = session.query(Topic).filter_by(id=topic_id).one()
+    note = session.query(Note).filter_by(topic_id=topic_id, id=note_id).one()
+    if (request.method == 'POST'):
+        note.title = request.form['title']
+        note.message = request.form['message']
+        session.commit()
+        return redirect(url_for('topic', topic_id=topic.id))
+    return render_template('edit-note.html', topic=topic, note=note)
+
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!meowmwow'
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
